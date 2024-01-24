5a68fa124a04523a7b5ea93a3db96b0b08cd5ea0
FelixJaeckel <fexlijaeckel@gmail.com>
2024-01-24 18:01:20 +0100

    aufgeräumt und ordentlich formattiert

--- main2.c
+++ main2.c
@@ -6,7 +6,7 @@
 #include <conio.h>
 #include <ctype.h>
 
-struct Uhrzeit; /* muss vor eigentlicher definition leer deklariert werden, damit der Prototyp Bescheid weiÃŸ, was eine Uhrzeit ist */
+struct Uhrzeit; /* muss vor eigentlicher definition leer deklariert werden, damit der Prototyp Bescheid weiss, was eine Uhrzeit ist */
 
 int cursorSetzen(HANDLE, unsigned short, unsigned short);
 void uhrzeitAnpassen(int);
@@ -45,78 +45,127 @@ int main(int argc, char *argv[]) {
 	bergstation_lift = 2;
 	tageskarten = 51;
 	int turbo = 0; // 0 = Sleep(1), 1 = Sleep(0.1)
-		PersonenGesamtaufBerg = 10;
+	PersonenGesamtaufBerg = 10;
 	VerkaufteZehnerKarten = 100;
 	VerkaufteTageskarten = 0;
 	Skifarten = 0;
 	SchneedorfGaesteAufDerPiste = 0;
 	ParkendeWagen = 0;
 	BuseDieHeuteDaWaren = 0;
-	/* loop lÃ¤uft bis 1320 Minuten, also bis 22:00 Uhr */
+	
+	/* loop laeuft bis 1320 Minuten, also bis 22:00 Uhr */
 	while(minuten <= 1320)
 	{
-			if(PersonenGesamtaufBerg>=100){
-	Leer1 = (" ");
-} else if(PersonenGesamtaufBerg>=10){
-	Leer1 = ("  ");
-} else {
-	Leer1 = ("   ");
-}
+		if(PersonenGesamtaufBerg>=100)
+		{
+			Leer1 = (" ");
+		} 
+		else if(PersonenGesamtaufBerg>=10)
+		{
+			Leer1 = ("  ");
+		} 
+		else 
+		{
+			Leer1 = ("   ");
+		}
 
-	if(VerkaufteZehnerKarten>=100){
-	Leer2 = (" ");
-} else if(VerkaufteZehnerKarten>=10){
-	Leer2 = ("  ");
-} else {
-	Leer2 = ("   ");
-}
 
-	if(VerkaufteTageskarten>=100){
-	Leer3 = (" ");
-} else if(VerkaufteTageskarten>=10){
-	Leer3 = ("  ");
-} else {
-	Leer3 = ("   ");
-}
-	if(VerkaufteTageskarten>=100){
-	Leer3 = (" ");
-} else if(VerkaufteTageskarten>=10){
-	Leer3 = ("  ");
-} else {
-	Leer3 = ("   ");
-}
+		if(VerkaufteZehnerKarten>=100){
+			Leer2 = (" ");
+		} 
+		else if(VerkaufteZehnerKarten>=10)
+		{
+			Leer2 = ("  ");
+		} 
+		else 
+		{
+			Leer2 = ("   ");
+		}
 
-	if(Skifarten>=100){
-	Leer4 = (" ");
-} else if(Skifarten>=10){
-	Leer4 = ("  ");
-} else {
-	Leer4 = ("   ");
-}
 
-	if(SchneedorfGaesteAufDerPiste>=100){
-	Leer5 = (" ");
-} else if(SchneedorfGaesteAufDerPiste>=10){
-	Leer5 = ("  ");
-} else {
-	Leer5 = ("   ");
-}
+		if(VerkaufteTageskarten>=100)
+		{
+			Leer3 = (" ");
+		} 
+		else if(VerkaufteTageskarten>=10)
+		{
+			Leer3 = ("  ");
+		} 
+		else 
+		{
+			Leer3 = ("   ");
+		}
+		
+		
+		if(VerkaufteTageskarten>=100)
+		{
+			Leer3 = (" ");
+		} 
+		else if(VerkaufteTageskarten>=10)
+		{
+			Leer3 = ("  ");
+		} 
+		else 
+		{
+			Leer3 = ("   ");
+		}
 
-	if(ParkendeWagen>=100){
-	Leer6 = (" ");
-} else if(ParkendeWagen>=10){
-	Leer6 = ("  ");
-} else {
-	Leer6 = ("   ");
-}
 
-	if(BuseDieHeuteDaWaren>=100){
-	Leer7 = (" ");
-} else if(BuseDieHeuteDaWaren>=10){
-	Leer7 = ("  ");
-} else {
-	Leer7 = ("   ");
-}
+		if(Skifarten>=100)
+		{
+			Leer4 = (" ");
+		} 
+		else if(Skifarten>=10)
+		{
+			Leer4 = ("  ");
+		} 
+		else 
+		{
+			Leer4 = ("   ");
+		}
+
+
+		if(SchneedorfGaesteAufDerPiste>=100)
+		{
+			Leer5 = (" ");
+		} 
+		else if(SchneedorfGaesteAufDerPiste>=10)
+		{
+			Leer5 = ("  ");
+		} 
+		else 
+		{
+			Leer5 = ("   ");
+		}
+	
+	
+		if(ParkendeWagen>=100)
+		{
+			Leer6 = (" ");
+		} 
+		else if(ParkendeWagen>=10)
+		{
+			Leer6 = ("  ");
+		} 
+		else 
+		{
+			Leer6 = ("   ");
+		}
+	
+	
+		if(BuseDieHeuteDaWaren>=100){
+			Leer7 = (" ");
+		} 
+		else if(BuseDieHeuteDaWaren>=10)
+		{
+			Leer7 = ("  ");
+		} 
+		else 
+		{
+			Leer7 = ("   ");
+		}
+	
+	
 		system("cls");
 		printf(	"10er-Karten:  %i                                   ___Bergstation    Lift ab:  %i\n"
 				"Tageskarten:  %i                                  /        |    |\n"
@@ -137,25 +186,25 @@ int main(int argc, char *argv[]) {
 				"                                     ----Talstation--               Lift auf:  2\n"
 				"  :   Uhr                               (H):  0\n"
 			   	"Personen auf Berg:  83                  [P]:  1 Auto\n"  
-				"\n                  ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»                "
-		"\n  ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄº                                          ºÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿"
-		"\n  ³               º      %sbersicht %sber den Berg             º               ³"
-		"\n  ÃÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄº                                          ºÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ´"
-		"\n  ³               ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼               ³"
-		"\n  ³                    ³           Allgemeine Zahlen:                        ³"
-		"\n  ³                    ³          Personen auf Berg:  %d%s                   ³"
-		"\n  ³                    ³          10er-Karten:  %d%s                         ³"
-		"\n  ³                    ³          Tageskarten: %d%s                          ³"
-		"\n  ³                    ³          Skifahrten:   %d%s                         ³"
-		"\n  ³                    ³          Schneedorf Besucher:   %d%s                ³"
-		"\n  ³                    ³          Parkende Wagen:  %d Auto %s                ³"
-		"\n  ³                    ³          Busse an diesem Tag:  %d  %s               ³"
-		"\n  ³                    ³          %s                                        ³"
-		"\n  ³                    ³          M%sgliche Eingaben:                         ³"
-		"\n  ³                    ³          (T)urbo                                    ³"
-		"\n  ³                    ³          (P)ause                                    ³"
-		"\n  ³                    ³          (0)  Programmende                          ³"
-		"\n  ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÁÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ" 
+				"\n                  Ã‰ÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÂ»                "
+		"\n  ÃšÃ„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Âº                                          ÂºÃ„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Â¿"
+		"\n  Â³               Âº      Ãœbersicht Ã¼ber den Berg             Âº               Â³"
+		"\n  ÃƒÃ„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Âº                                          ÂºÃ„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Â´"
+		"\n  Â³               ÃˆÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÂ¼               Â³"
+		"\n  Â³                    Â³           Allgemeine Zahlen:                        Â³"
+		"\n  Â³                    Â³          Personen auf Berg:  %d%s                   Â³"
+		"\n  Â³                    Â³          10er-Karten:  %d%s                         Â³"
+		"\n  Â³                    Â³          Tageskarten: %d%s                          Â³"
+		"\n  Â³                    Â³          Skifahrten:   %d%s                         Â³"
+		"\n  Â³                    Â³          Schneedorf Besucher:   %d%s                Â³"
+		"\n  Â³                    Â³          Parkende Wagen:  %d Auto %s                Â³"
+		"\n  Â³                    Â³          Busse an diesem Tag:  %d  %s               Â³"
+		"\n  Â³                    Â³          %s                                        Â³"
+		"\n  Â³                    Â³          M%sgliche Eingaben:                         Â³"
+		"\n  Â³                    Â³          (T)urbo                                    Â³"
+		"\n  Â³                    Â³          (P)ause                                    Â³"
+		"\n  Â³                    Â³          (0)  Programmende                          Â³"
+		"\n  Ã€Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„ÃÃ„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã„Ã™" 
 		"                                                                 \n"
 		"                                                                 \n"
 		"                                                                 \n"
@@ -170,36 +219,50 @@ int main(int argc, char *argv[]) {
 		bergstation_lift++; 
 		tageskarten++;
 	
-		cursorSetzen(hStdout, 0, 24); /* setzt Cursor an den Anfang, damit Ausgabe scheinbar konstant bleibt */
-	if (turbo) {
+		cursorSetzen(hStdout, 0, 0); /* setzt Cursor an den Anfang, damit Ausgabe scheinbar konstant bleibt */
+		
+		if (turbo) 
+		{
             usleep(100000); // Wenn turbo Modus sleep 0.1
-        } else {
+        } 
+		else 
+		{
             sleep(1); // sleep 1 sekunden im normalen modus
         }
+        
 		minuten++; /* eine minute vergeht */
 		uhrzeitAnpassen(minuten); /* minuten werden in uhrzeitformat umgewandelt */
-		if (kbhit()) { // Überprüfen, ob eine Taste gedrückt wurde
+		
+		if (kbhit())  // ÃœberprÃ¼fen, ob eine Taste gedrÃ¼ckt wurde
+		{
             input = tolower(getchar()); // Benutzereingabe lesen
-            //Turbo
-             if (input == 't') { // Turbo aktiveren/Deaktivieren
+            
+			//Turbo
+            if (input == 't') // Turbo aktiveren/Deaktivieren
+			{ 
                 turbo = !turbo; // Wechsel des Turbo moduses
                 printf("Turbo-Modus %s\n", turbo ? "aktiviert" : "deaktiviert");
             }
-            //Pause
-            if (input == 'p') { // Falls 'p' gedrückt wurde, pausieren
-                printf("Die Uhr ist pausiert. Drücken Sie 'r', um fortzufahren.\n");
-                while (1) {
+            
+			//Pause
+            if (input == 'p') // Falls 'p' gedrÃ¼ckt wurde, pausieren
+			{ 
+                printf("Die Uhr ist pausiert. DrÃ¼cken Sie 'r', um fortzufahren.\n");
+                while (1) 
+				{
                     input = getchar();
-                    if (input == 'r') { // Falls 'r' gedrückt wurde, fortsetzen
-                        printf("Die Uhr läuft weiter\n");
+                    if (input == 'r') // Falls 'r' gedrÃ¼ckt wurde, fortsetzen
+					{ 
+                        printf("Die Uhr lÃ¤uft weiter\n");
                         break;
                         fflush(stdin);
                     }
                 }
             }
-            else if (input == '0') { // Progamm beendung mit 0
+            else if (input == '0') // Progamm beendung mit 0
+			{ 
                   
-				  return;
+				  break;
             }
         }
 	} 	
@@ -223,13 +286,13 @@ void uhrzeitAnpassen(int minuten)
 }
 
 
-/* sorgt dafÃ¼r, dass uhrzeit immer mit zwei Ziffern vor und hinter dem Doppelpunkt ausgegeben wird */
+/* sorgt dafuer, dass uhrzeit immer mit zwei Ziffern vor und hinter dem Doppelpunkt ausgegeben wird */
 void uhrzeitAusgeben(Uhrzeit uhrzeit)
 {
 	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); /* wird fuer cursorSetzen() benoetigt */
 	
 	/* packt 0 vor die Stunde wenn es um 9 ist, ansonsten wird die Stunde einfach nur geprintet */
-	cursorSetzen(hStdout, 0, 2);	
+	cursorSetzen(hStdout, 0, 17);	
 	if (uhrzeit.stunde == 9) 
 	{
 		printf("0%i", uhrzeit.stunde);
@@ -240,7 +303,7 @@ void uhrzeitAusgeben(Uhrzeit uhrzeit)
 	}
 	
 	/* packt 0 vor einstellige Minuten */
-	cursorSetzen(hStdout, 3, 2);	
+	cursorSetzen(hStdout, 3, 17);	
 	if (uhrzeit.minute >= 10) 
 	{
 		printf("%i", uhrzeit.minute);
@@ -253,8 +316,8 @@ void uhrzeitAusgeben(Uhrzeit uhrzeit)
 }
 
 
-/* Portenzelle Schon vorbereiteter Code Falls er noch verwennendbar ist
-Vorbereitete Skilift Variabeln
+/* Portenzelle Schon vorbereiteter Code Falls er noch verwennendbar ist */
+/* Vorbereitete Skilift Variabeln 
 int B1, B2, R1, R2, S1;//Pisten
 int WarteschlangeBergstationAbwaerz, WarteschlangeMittelstationAufwaerz, WarteschlangeMittelstationAbwaerz , WarteschlangeTalstationAufwaerz;//Warteschlangen
 int LiftfahrerMittelstationBergstation, LiftfahrerTalstationMittelstation;//Liftbenutzer
@@ -272,98 +335,92 @@ LiftfahrerMittelstationBergstation = 0;
 LiftfahrerTalstationMittelstation = 0;
 BistroGaeste = 0;
 
-Busankunft & Abfahrten
-if(t1.stunde == 8 & t1.minute == 30 || t1.stunde == 8 & t1.minute == 50 ||
-t1.stunde == 9 & t1.minute == 10 || t1.stunde == 9 & t1.minute == 30 || t1.stunde == 9 & t1.minute == 50 ||
-t1.stunde == 10 & t1.minute == 10 || t1.stunde == 10 & t1.minute == 30 || t1.stunde == 10 & t1.minute == 50 ||
-t1.stunde == 11 & t1.minute == 10 || t1.stunde == 11 & t1.minute == 30 || t1.stunde == 11 & t1.minute == 50 ||
-t1.stunde == 12 & t1.minute == 10 || t1.stunde == 12 & t1.minute == 30 || t1.stunde == 12 & t1.minute == 50 ||
-t1.stunde == 13 & t1.minute == 10 || t1.stunde == 13 & t1.minute == 30 || t1.stunde == 13 & t1.minute == 50 ||
-t1.stunde == 14 & t1.minute == 10 || t1.stunde == 14 & t1.minute == 30 || t1.stunde == 14 & t1.minute == 50 ||
-t1.stunde == 15 & t1.minute == 10 || t1.stunde == 15 & t1.minute == 30 || t1.stunde == 15 & t1.minute == 50 ||
-t1.stunde == 16 & t1.minute == 10 || t1.stunde == 16 & t1.minute == 30 || t1.stunde == 16 & t1.minute == 50 ||
-t1.stunde == 17 & t1.minute == 10 || t1.stunde == 17 & t1.minute == 30 || t1.stunde == 17 & t1.minute == 50 ||
-t1.stunde == 18 & t1.minute == 10 || t1.stunde == 18 & t1.minute == 30 || t1.stunde == 18 & t1.minute == 50 ||
-t1.stunde == 19 & t1.minute == 10 || t1.stunde == 19 & t1.minute == 30 || t1.stunde == 19 & t1.minute == 50 ||
-t1.stunde == 20 & t1.minute == 10){
+/* Busankunft & Abfahrten 
+
+if (uhrzeit.minute == 10 ||uhrzeit.minute == 30 ||uhrzeit.minute == 50)
+{
 	Busn = ("Der bus ist hier.");
-} else if(t1.stunde == 8 & t1.minute == 33 || t1.stunde == 8 & t1.minute == 53 ||
-t1.stunde == 9 & t1.minute == 13 || t1.stunde == 9 & t1.minute == 33 || t1.stunde == 9 & t1.minute == 53 ||
-t1.stunde == 10 & t1.minute == 13 || t1.stunde == 10 & t1.minute == 33 || t1.stunde == 10 & t1.minute == 53 ||
-t1.stunde == 11 & t1.minute == 13 || t1.stunde == 11 & t1.minute == 33 || t1.stunde == 11 & t1.minute == 53 ||
-t1.stunde == 12 & t1.minute == 10 || t1.stunde == 12 & t1.minute == 33 || t1.stunde == 12 & t1.minute == 53 ||
-t1.stunde == 13 & t1.minute == 13 || t1.stunde == 13 & t1.minute == 33 || t1.stunde == 13 & t1.minute == 53 ||
-t1.stunde == 14 & t1.minute == 13 || t1.stunde == 14 & t1.minute == 33 || t1.stunde == 14 & t1.minute == 53 ||
-t1.stunde == 15 & t1.minute == 13 || t1.stunde == 15 & t1.minute == 33 || t1.stunde == 15 & t1.minute == 53 ||
-t1.stunde == 16 & t1.minute == 13 || t1.stunde == 16 & t1.minute == 33 || t1.stunde == 16 & t1.minute == 53 ||
-t1.stunde == 17 & t1.minute == 13 || t1.stunde == 17 & t1.minute == 33 || t1.stunde == 17 & t1.minute == 53 ||
-t1.stunde == 18 & t1.minute == 13 || t1.stunde == 18 & t1.minute == 33 || t1.stunde == 18 & t1.minute == 53 ||
-t1.stunde == 19 & t1.minute == 13 || t1.stunde == 19 & t1.minute == 33 || t1.stunde == 19 & t1.minute == 53 ||
-t1.stunde == 20 & t1.minute == 13){
+} 
+else if(uhrzeit.minute == 13 || uhrzeit.minute == 33 || uhrzeit.minute == 53)
+{
 	Busn = ("Der Bus ist abgefahren.");
-} else {
+} 
+else 
+{
 	Busn = ("...");
 }
 
 // Zahlen runschiebe
 //Holle zufalls zahl von 0-2 Leuten aus schneedorf
-int getZufallszahl() {
+int getZufallszahl() 
+{
     return rand() % 3; // ergibt eine Zufallszahl zwischen 0 und 2
 }
 
-//Holle Zufallszahl von 1-5 für Leute die im Auto kommen
-int getZufallszahl() {
-    int getRandomNumber() {
-    return (rand() % 5) + 1; // ergibt eine Zufallszahl zwischen 0 und 4, daher wird 1 addiert
-}
+//Holle Zufallszahl von 1-5 fÃ¼r Leute die im Auto kommen
+int getZufallszahl() 
+{
+    int getRandomNumber() 
+	{
+    	return (rand() % 5) + 1; // ergibt eine Zufallszahl zwischen 0 und 4, daher wird 1 addiert
+	}
 }
 
-//Holle Zufallszahl von 0-50 für Leute die mit dem Bus kommen
-int getRandomNumber() {
+//Holle Zufallszahl von 0-50 fÃ¼r Leute die mit dem Bus kommen
+int getRandomNumber() 
+{
     return rand() % 51; //ergibt eine Zufallszahl zwischen 0 und 50
 }
 
 //Zwischen 9:00 und 11 uhr kommen leute aus dem dorf
-void getinSchneedorfgeaste(){
+void getinSchneedorfgeaste()
+{
 	int ZufallsZahlSchneeGaesteJetzt;
 	ZufallsZahlSchneeGaesteJetzt = 0;
-	if (SchneedorfGaesteAufDerPiste <= 58){
-	if( 9 < t.stunde < 11 ){
-		getZufallszahl() = ZufallsZahlSchneeGaesteJetzt;
-		PersonenGesamtaufBerg = PersonenGesamtaufBerg + ZufallsZahlSchneeGaesteJetzt;
-		SchneedorfGaesteAufDerPiste = SchneedorfGaesteAufDerPiste + ZufallsZahlSchneeGaesteJetzt;
-}
-}
+	
+	if (SchneedorfGaesteAufDerPiste <= 58)
+	{
+		if( 9 < t.stunde < 11 )
+		{
+			getZufallszahl() = ZufallsZahlSchneeGaesteJetzt;
+			PersonenGesamtaufBerg = PersonenGesamtaufBerg + ZufallsZahlSchneeGaesteJetzt;
+			SchneedorfGaesteAufDerPiste = SchneedorfGaesteAufDerPiste + ZufallsZahlSchneeGaesteJetzt;
+		}
+	}
 }
 //Zwischen 17:00 und 22 uhr gehen leute aus dem dorf
-void getoutSchneedorfgeaste(){
+void getoutSchneedorfgeaste()
+{
 	int ZufallsZahlSchneeGaesteJetzt;
 	ZufallsZahlSchneeGaesteJetzt = 0;
-	if (SchneedorfGaesteAufDerPiste = 0){
-	if( 17 < t.stunde < 22 ){
-		getZufallszahl() = ZufallsZahlSchneeGaesteJetzt;
-		PersonenGesamtaufBerg = PersonenGesamtaufBerg - ZufallsZahlSchneeGaesteJetzt;
-		SchneedorfGaesteAufDerPiste = SchneedorfGaesteAufDerPiste - ZufallsZahlSchneeGaesteJetzt;
-}
-}
+	if (SchneedorfGaesteAufDerPiste = 0)
+	{
+		if( 17 < t.stunde < 22 )
+		{
+			getZufallszahl() = ZufallsZahlSchneeGaesteJetzt;
+			PersonenGesamtaufBerg = PersonenGesamtaufBerg - ZufallsZahlSchneeGaesteJetzt;
+			SchneedorfGaesteAufDerPiste = SchneedorfGaesteAufDerPiste - ZufallsZahlSchneeGaesteJetzt;
+		}
+	}
 }
 
-void getschneewache(){
-SchneedorfGaesteAufDerPiste = 0;
-ParkendeWagen =0;
-PersonenGesamtaufBerg = 0;
-
-B1 = 0;
-B2 = 0;
-R1 = 0;
-R2 = 0;
-S1 = 0;
-WarteschlangeBergstationAbwaerz = 0;
-WarteschlangeMittelstationAufwaerz = 0;
-WarteschlangeMittelstationAbwaerz = 0;
-WarteschlangeTalstationAufwaerz = 0;
-LiftfahrerMittelstationBergstation = 0;
-LiftfahrerTalstationMittelstation = 0;
-BistroGaeste = 0;
+void getschneewache()
+{
+	SchneedorfGaesteAufDerPiste = 0;
+	ParkendeWagen = 0;
+	PersonenGesamtaufBerg = 0;
+	
+	B1 = 0;
+	B2 = 0;
+	R1 = 0;
+	R2 = 0;
+	S1 = 0;
+	WarteschlangeBergstationAbwaerz = 0;
+	WarteschlangeMittelstationAufwaerz = 0;
+	WarteschlangeMittelstationAbwaerz = 0;
+	WarteschlangeTalstationAufwaerz = 0;
+	LiftfahrerMittelstationBergstation = 0;
+	LiftfahrerTalstationMittelstation = 0;
+	BistroGaeste = 0;
 }
 */
