6467d8ed1027049d4aeee5a0cc136cc49c6e0702
FelixJaeckel <fexlijaeckel@gmail.com>
2024-01-24 18:02:52 +0100

    Turbo, Pause und beenden V1, input muss überarbeitet werden

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <conio.h>
 
 struct Uhrzeit; /* muss vor eigentlicher definition leer deklariert werden, damit der Prototyp Bescheid weiß, was eine Uhrzeit ist */
 
@@ -19,13 +20,16 @@ Uhrzeit uhrzeit; /* uhrzeit ist globale Variable, um UhrzeitAnpassen() zu erleic
 
 int main(int argc, char *argv[]) {
 	
-	int zehnerkarten, bergstation_lift, tageskarten, minuten;
+	int zehnerkarten, bergstation_lift, tageskarten, minuten, loop_anzahl;
+	char input;
 	
 	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); /* wird fuer cursorSetzen() benoetigt */
 	
 	minuten = 540; /* wir starten bei 540 Minuten, weil der Skilift 9:00 Uhr aufmacht */
 	uhrzeit.stunde = 9; 
 	uhrzeit.minute = 0;
+	
+	loop_anzahl = 1; /* bestimmt, wie oft pro Sekunde der Hauptloop durchlaufen wird. Standardwert ist 1, Turbo ist 10, Pause ist 0 */
 	
 	/* Variablen zu Testzwecken */
 	zehnerkarten = 15;
@@ -35,40 +39,93 @@ int main(int argc, char *argv[]) {
 	/* loop läuft bis 1320 Minuten, also bis 22:00 Uhr */
 	while(minuten <= 1320)
 	{
-		printf( "10er-Karten:  %i                                   ___Bergstation    Lift ab:  %i\n"
-				"Tageskarten:  %i                                  /        |    |\n"
-				"Skifahrten:   77                                 /        /     |\n"
-				"                                                -        /      |\n"
-				"                                               /        |       |\n"
-				"Lift M<>B:  22                               B2:  6   R2:   8  /\n"
-				"                                             /        /       /\n"
-				"                                         ----        |       /\n"
-				"                                        /           /       /       Lift auf:  1\n"
-				"                                        Mittelstation      |\n"
-				"                                   _____Bistro:  7        /         Lift ab:   0\n"
-				"                                  /       \\             |\n"
-				"                                  \\        |           /\n"
-				"Lift T<>M:  14                   B1:  4    R1: 12     S1:  5\n"
-				"                                   \\        \\        /\n"
-				"                                    \\       /       /\n"
-				"                                     ----Talstation--               Lift auf:  2\n"
-				"  :   Uhr                               (H):  0\n"
-			   	"Personen auf Berg:  83                  [P]:  1 Auto\n"
-				"...(T)urbo\n"
-				"...(P)ause"                                                
-				, zehnerkarten, bergstation_lift, tageskarten);
-		
-		uhrzeitAusgeben(uhrzeit); /* uhrzeit hat ein paar Eigenheiten, weswegen sie eine extrafunktion zum printen kriegt */
-		
-		/* nur zu testzwecken hier */
-		zehnerkarten++;
-		bergstation_lift++; 
-		tageskarten++;
-		
-		cursorSetzen(hStdout, 0, 0); /* setzt Cursor an den Anfang, damit Ausgabe scheinbar konstant bleibt */
+		for (int i = 0; i < loop_anzahl; i++)
+		{
+			printf( "10er-Karten:  %i                                   ___Bergstation    Lift ab:  %i\n"
+					"Tageskarten:  %i                                  /        |    |\n"
+					"Skifahrten:   77                                 /        /     |\n"
+					"                                                -        /      |\n"
+					"                                               /        |       |\n"
+					"Lift M<>B:  22                               B2:  6   R2:   8  /\n"
+					"                                             /        /       /\n"
+					"                                         ----        |       /\n"
+					"                                        /           /       /       Lift auf:  1\n"
+					"                                        Mittelstation      |\n"
+					"                                   _____Bistro:  7        /         Lift ab:   0\n"
+					"                                  /       \\             |\n"
+					"                                  \\        |           /\n"
+					"Lift T<>M:  14                   B1:  4    R1: 12     S1:  5\n"
+					"                                   \\        \\        /\n"
+					"                                    \\       /       /\n"
+					"                                     ----Talstation--               Lift auf:  2\n"
+					"  :   Uhr                               (H):  0\n"
+				   	"Personen auf Berg:  83                  [P]:  1 Auto\n"
+					"...(T)urbo\n"
+					"...(P)ause"                                                
+					, zehnerkarten, bergstation_lift, tageskarten);
+			
+			uhrzeitAusgeben(uhrzeit); /* uhrzeit hat ein paar Eigenheiten, weswegen sie eine extrafunktion zum printen kriegt */
+			
+			/* nur zu testzwecken hier */
+			zehnerkarten++;
+			bergstation_lift++; 
+			tageskarten++;
+			
+			minuten++; /* eine minute vergeht */
+			uhrzeitAnpassen(minuten); /* minuten werden in uhrzeitformat umgewandelt */		
+			cursorSetzen(hStdout, 0, 0); /* setzt Cursor an den Anfang, damit Ausgabe scheinbar konstant bleibt */	
+		}
+
 		Sleep(1000); /* wartet eine sekunde */
-		minuten++; /* eine minute vergeht */
-		uhrzeitAnpassen(minuten); /* minuten werden in uhrzeitformat umgewandelt */
+		
+		if (kbhit())  // Überprüfen, ob eine Taste gedrückt wurde
+		{
+            input = tolower(getchar()); // Benutzereingabe lesen
+            
+			//Turbo
+            if (input == 't') // Turbo aktiveren/Deaktivieren
+			{
+				if (loop_anzahl != 10) /* wenn Turbo aus ist, wechsel zu Turbo */
+				{
+				    loop_anzahl = 10; // Wechsel des Turbo moduses	
+				} 
+				else /* wenn Turbo an ist, wechsel zu Standard */
+				{
+					loop_anzahl = 1; 
+				}
+                /* printf("Turbo-Modus %s\n", turbo ? "aktiviert" : "deaktiviert"); */
+            }
+            
+			//Pause
+            if (input == 'p') // Falls 'p' gedrückt wurde, pausieren
+			{ 
+				if (loop_anzahl != 0) /* wenn Pause aus ist, wechsel zu Pause */
+				{
+					loop_anzahl = 0;
+				} 
+				else /* wenn Pause an ist, wechsel zu Standard */
+				{
+					loop_anzahl = 1;
+				}
+                /* printf("Die Uhr ist pausiert. Drücken Sie 'r', um fortzufahren.\n"); */
+                /* while (1) 
+				{
+                    input = tolower(getchar();
+                    if (input == 'p') // Falls 'r' gedrückt wurde, fortsetzen
+					{ 
+                        break;
+                        fflush(stdin);
+                    }
+                } */
+            }
+            else if (input == '0') // Progamm beendung mit 0
+			{ 
+                  
+				  break;
+            }
+        }
+		
+		
 	} 	
 	return 0;
 }
